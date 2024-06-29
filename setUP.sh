14f472cc249226cf1daa936b2cf81752d85a7147
FazalKadivar21 <fazalkadivar7@gmail.com>
2024-06-29 08:30:11 +0000

    first commit

---
+++ setUP.sh
@@ -0,0 +1,12 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt-get install git 
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+curl -sS https://starship.rs/install.sh | sh
+echo "eval "$(starship init zsh)"" >> ~/.zshrc
+starship preset gruvbox-rainbow -o ~/.config/starship.toml
