0668f4f939e4b42471c8bf1c8cc396736933a167
FazalKadivar21 <fazalkadivar7@gmail.com>
2024-06-29 08:35:02 +0000

    initial commit

--- setUP.sh
+++ setUP.sh
@@ -1,12 +1,29 @@
+#!/bin/bash
+
+# Update and upgrade the system
 sudo apt update && sudo apt upgrade -y
-sudo apt-get install git 
-sudo apt install zsh
+
+# Install Git
+sudo apt-get install -y git
+
+# Install Zsh
+sudo apt install -y zsh
+
+# Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Clone zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+echo "source $(pwd)/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# Clone zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+# Install Starship prompt
 curl -sS https://starship.rs/install.sh | sh
-echo "eval "$(starship init zsh)"" >> ~/.zshrc
+echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+
+# Apply Starship preset
 starship preset gruvbox-rainbow -o ~/.config/starship.toml
